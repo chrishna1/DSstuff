36c5641f470572c11f207429e241f57153f30318
chrishna1 <adjnct@gmail.com>
2018-06-01 12:54:51 +0530

    Update and rename intro to intro.py

---
+++ plottinglibrary/matplotlib/intro.py
@@ -0,0 +1,3 @@
+#rcParams- configuration files(also known as run commands) contains all the default parameters when matplotlib are used.
+matplotlib.rcParams # list all default params
+#common rcParams - image.cmap,image.interpolation,figure.figsize
